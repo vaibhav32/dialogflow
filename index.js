5a7480a7ef2fcebe9650df20b666881018d92f92
Vaibhav Singh <singh.vaibhav32@gmail.com>
2018-07-17 12:19:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var express=require('express');
 var app=express();
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.get("/",function(req,res){
 	res.send("helo welcome to heroku...");
